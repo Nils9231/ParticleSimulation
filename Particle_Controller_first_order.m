bb61c397775de170a46219dae8c91ed37c6e0ca7
Nils Timmermann <nils.timmermann@tuhh.de>
2018-07-26 19:25:09 +0200

    Added linear Trajectory following

--- Particle_Controller_first_order.m
+++ Particle_Controller_first_order.m
@@ -4,7 +4,7 @@
 %   Motion"
 %
 %   first order steering
-
+close all
 %% Deklarations/Definitions
 % Timestepsize
 dt = 0.01;
@@ -24,17 +24,25 @@ theta_abs(1,:) = 1:N;
 % Matrix of relative orientations
 %theta_rel = zeros (N,N);
 % Position differences
-rx = zeros(1,N);
-ry = zeros(1,N);
+drx = zeros(1,N);
+dry = zeros(1,N);
 % Time-Derivation of Speed Orientation 
 nu = zeros (1,N);
-% Control Gain 
-K = -1;
+% Control Gains 
+K = -1; % Swarm Gain
+Kt = 4;   % Tangetial Gain
 %   K<0 for straight-line Motion (parrallel trajectories)
 %   K>0 for balanced Motion (Sum of all velocities equal zero)
-% if straight Line Motion target Angle:
-theta_target = 0;
+% if straight Line Motion Tangent_target:
+r_Tx=-20:0.01:20;
+r_Ty= 1* r_Tx;
+r_T=[1;1];
 
+theta_T = atan2(r_T(2),r_T(1));
+% Punkte auf Tangente, mit minimalem Abstand zum  Boot
+R_T = zeros(2,1);
+% Vektor zur Tangente
+r_target = zeros(2,1);
 % Circular Motion?
 circular_motion = 0; % 1 Yes 0 No
 
@@ -54,24 +62,31 @@ if K<0
     ylabel('y (m)');
     for ii = 1:(tmax/dt)
         nu = zeros(1,N);
-
-
 %         for jj = 1:N
 %             for kk = 1:N
 %                 nu(jj) = nu(jj) + sin(theta_abs(ii,kk)-theta_abs(ii,jj));
 %             end
 %         end 
+%         nu = - (K/N)*nu;
         for jj = 1:N
-            nu(jj) = nu(jj) + sin(theta_target-theta_abs(ii,jj));
+            R_T = [norm([r(2*ii-1,jj);r(2*ii,jj)])*sin(theta_T)*cos(theta_T);norm([r(2*ii-1,jj);r(2*ii,jj)])*sin(theta_T)*sin(theta_T)]+r_T*Kt;
+            r_target = R_T-[r(2*ii-1,jj);r(2*ii,jj)];
+            %theta_target = acos(r_target(1)/norm(r_target));
+            theta_target = atan2(r_target(2),r_target(1));
+            nu(jj) = sin(theta_target-theta_abs(ii,jj));
         end
         nu = - (K)*nu;
+        
+        %Winkel zur Tangente berechnen
+        
+        
         theta_abs(ii+1,:) = theta_abs(ii,:)+ nu * dt ;
+        
+        drx = cos(theta_abs(ii,:))*v*dt;
+        dry = sin(theta_abs(ii,:))*v*dt;
 
-        rx = cos(theta_abs(ii,:))*v*dt;
-        ry = sin(theta_abs(ii,:))*v*dt;
-
-        r(2*(ii+1)-1,:)= r(2*ii-1,:)+rx;
-        r(2*(ii+1),:)= r(2*ii,:)+ry;
+        r(2*(ii+1)-1,:)= r(2*ii-1,:)+drx;
+        r(2*(ii+1),:)= r(2*ii,:)+dry;
 
         plot(r(2*ii-1,:),r(2*ii,:),'b.');
 
@@ -98,11 +113,11 @@ elseif K>0
         nu = - (K/N)*nu;
         theta_abs(ii+1,:) = theta_abs(ii,:)+ nu * dt ;
 
-        rx = cos(theta_abs(ii,:))*v*dt;
-        ry = sin(theta_abs(ii,:))*v*dt;
+        drx = cos(theta_abs(ii,:))*v*dt;
+        dry = sin(theta_abs(ii,:))*v*dt;
 
-        r(2*(ii+1)-1,:)= r(2*ii-1,:)+rx;
-        r(2*(ii+1),:)= r(2*ii,:)+ry;
+        r(2*(ii+1)-1,:)= r(2*ii-1,:)+drx;
+        r(2*(ii+1),:)= r(2*ii,:)+dry;
 
         plot(r(2*ii-1,:),r(2*ii,:),'b.');
 
@@ -136,11 +151,11 @@ elseif circular_motion == 1
         
         theta_abs(ii+1,:) = theta_abs(ii,:)+ nu * dt ;
 
-        rx = cos(theta_abs(ii,:))*v*dt;
-        ry = sin(theta_abs(ii,:))*v*dt;
+        drx = cos(theta_abs(ii,:))*v*dt;
+        dry = sin(theta_abs(ii,:))*v*dt;
 
-        r(2*(ii+1)-1,:)= r(2*ii-1,:)+rx;
-        r(2*(ii+1),:)= r(2*ii,:)+ry;
+        r(2*(ii+1)-1,:)= r(2*ii-1,:)+drx;
+        r(2*(ii+1),:)= r(2*ii,:)+dry;
         
         c(2*(ii+1)-1,:) = r(2*(ii+1)-1,:) - 1/ome_0*sin(theta_abs(ii+1,:));
         c(2*(ii+1),:) = r(2*(ii+1),:) + 1/ome_0*cos(theta_abs(ii+1,:));
